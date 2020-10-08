a5a0d4793c95d589abc63e7d077377cf18db069a
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-08 15:26:34 -0400

    working with primitive data types

---
+++ primitivetypes.java
@@ -0,0 +1,17 @@
+public class primitivetypes {
+    public static void main(String[] args) {
+        //primitive types
+        //byte 1 byte
+        //short 2 byte
+        //int 4 byt
+        //long 8 byte
+        //float 4 byte
+        //double 8 byte
+        //char 2 byte
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A'; //single characters with single quotes
+        boolean isElegible = false;
+    }
+}
