d3c6a02f43bfc827f0e01f4cd5cc08c4502c14a8
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-08 14:54:22 -0400

    basic understanding of variables

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        int age = 30; //initializing value
+        age = 22;
+
+        int myAge = 22; //camel case for variables
+        int herAge = myAge;
+        System.out.println(age);
+        System.out.println(herAge);
+    }
+}
