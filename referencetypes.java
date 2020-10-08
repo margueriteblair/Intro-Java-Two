f695c95c7a3fadef55c51684126c754cb875ad23
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-08 15:32:45 -0400

    reference types to get date

---
+++ referencetypes.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class referencetypes {
+    public static void main(String[] args) {
+        //reference types store complex objects
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
