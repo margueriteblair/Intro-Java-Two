63c0c68880bb11febaa9f4373b2da80d042b8eca
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-08 22:23:58 -0400

    working math class method

---
+++ mathclass.java
@@ -0,0 +1,10 @@
+public class mathclass {
+    public static void main(String[] args) {
+        int result = Math.round(3.2F);
+        Math.ceil(3.1);
+        Math.floor(1.9);
+        Math.max(2, 4);
+        double result2 = Math.random(); //math.random returns a double
+        System.out.println(result);
+    }
+}
