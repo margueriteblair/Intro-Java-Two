7c9c5137d72955e7651d5523902e25da617143c8
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-08 20:52:52 -0400

    strings

---
+++ strings.java
@@ -0,0 +1,8 @@
+public class strings {
+    public static void main(String[] args) {
+        String message = "Hello world" + "!!";
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message);
+    }
+}
