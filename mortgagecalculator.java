32e7c6b5dc6e6f3b51949b1926764c055d3f1bbd
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-09 13:11:50 -0400

    also refacted with moshs version

--- mortgagecalculator.java
+++ mortgagecalculator.java
@@ -1,3 +1,6 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class mortgagecalculator {
     public static void main(String[] args) {
         //Principal: 100,000
@@ -14,5 +17,30 @@ public class mortgagecalculator {
         r = r/ (double)100 /(double)12; //this is the monthly interest rate
         maturity = (principal*(r*(Math.pow(1+r, maturity)))/(Math.pow((1+r), maturity) - 1));
         System.out.println(maturity);
+
+        //Mosh's version
+
+        final byte MONTHS_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's the principal amount?");
+        int principalMosh = scanner.nextInt();
+
+        System.out.println("Annual interest rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_YEAR;
+
+        double mortgage = principal
+                        * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) /
+                        (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+
+        System.out.println(mortgageFormatted);
+
     }
 }
