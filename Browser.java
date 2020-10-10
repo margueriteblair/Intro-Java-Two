9a7836d54c753ba6ba5d943c39b10e6b54cdac31
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-10 13:58:33 -0400

    reduced coupling

---
+++ Browser.java
@@ -0,0 +1,18 @@
+public class Browser {
+    public void navigate(String addr) {
+        String ip = findIPAddress(addr);
+        String html = sendHttpRequest(ip);
+    }
+
+    private String sendHttpRequest(String ip) {
+        return "<html></html>";
+    }
+
+    private String findIPAddress(String address) {
+        return "127.0.0.1";
+    }
+}
+//findIpAddress and sendHttpRequest are both private methods because they are passed into navigate method
+//we won't need to see the other two methods
+//this is abstraction
+//this is also reducing coupling
