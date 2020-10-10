c2b2931ea5b1f679571d3bf72a6056d227e624c7
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-10 19:01:22 -0400

    created the console class

---
+++ Mortgage-Calculator/src/Browser.java
@@ -0,0 +1,6 @@
+public class Browser {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x);
+    }
+}
---
+++ Mortgage-Calculator/src/Console.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner = new Scanner(System.in); //making a private scanner local to the class, not an instance
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max) {
+                break;
+            }
+            System.out.println("Enter a value between" + min + "and" + max);
+        }
+        return value;
+    }
+}
