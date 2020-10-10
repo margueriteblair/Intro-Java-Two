46cc241e066b328d42793fffe04a56c19ff8b748
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-09 22:19:01 -0400

    importing classes

--- createclass.java
+++ createclass.java
@@ -1,5 +1,11 @@
 public class createclass {
     public static void main(String[] args) {
-        
+        var textBox1 = new TextBox();
+        textBox1.setText("Box1");
+
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
     }
 }
