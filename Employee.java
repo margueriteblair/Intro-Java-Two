c70b6343793496a06acd7b5e724fcb4e1f2583a6
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-10 18:16:07 -0400

    fully refactorded employee code

--- Employee.java
+++ Employee.java
@@ -27,5 +27,27 @@ public class Employee {
     public int calculateWage() {
         return calculateWage(0); //this is method overloading
     }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+            this.baseSalary = baseSalary;
+        }
+
+    }
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    private void setHourlyRate(int hourlyRate) {
+        if (hourlyRate < 0) {
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or negative.");
+            this.hourlyRate = hourlyRate;
+        }
+    }
 }
 
