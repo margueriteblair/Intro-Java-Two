75e42489aaca95a9444a2fdf1855bd70ad65fc27
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-09 22:08:00 -0400

    created a class

---
+++ TextBox.java
@@ -0,0 +1,11 @@
+public class TextBox {
+        public String text; //this is a field
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public void clear() {
+            this.text = "";
+        }
+}
---
+++ createclass.java
@@ -0,0 +1,5 @@
+public class createclass {
+    public static void main(String[] args) {
+        
+    }
+}
