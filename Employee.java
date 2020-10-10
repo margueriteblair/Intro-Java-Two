383537e2027ed100a3158735759a12cd037a9400
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-10 13:02:01 -0400

    updated Employee class

--- Employee.java
+++ Employee.java
@@ -1,8 +1,32 @@
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
-    public int calculateWage(int extraHours) {
+    public void setBaseSalary(final int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("salary cannot be 0 or less");
+            this.baseSalary = baseSalary;
+        }
+
+        public int getBaseSalary() {
+            return baseSalary;
+        }
+
+        public int getHourlyRate(){
+            return hourlyRate;
+        }
+
+        public void setHourlyRate(final int hourlyRate) {
+            if (hourlyRate <= 0) {
+                throw new IllegalArgumentException("Hourly rate cannot be less than 0.");
+            }
+            this.hourlyRate = hourlyRate;
+        }
+
+
+    }
+
+    public int calculateWage(final int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
 }
