55f9bebf48168e7fb5b75702539888030c4ac42d
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-10 20:02:34 -0400

    added comments and notes to console

--- Mortgage-Calculator/src/Console.java
+++ Mortgage-Calculator/src/Console.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
-
+//console class is specifically for reading data from the console
+//this is the only time we'll be using a scanner
 public class Console {
     private static Scanner scanner = new Scanner(System.in); //making a private scanner local to the class, not an instance
+//this scanner instance is only used internally inside this console class
 
     public static double readNumber(String prompt) {
         return scanner.nextDouble();
+        //two static methods, because we only have one console
+        //overloading this method
     }
 
     public static double readNumber(String prompt, double min, double max) {
