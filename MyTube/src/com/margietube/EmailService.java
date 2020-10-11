b113aa0647eee92b54d8e9805b07ce89e80b2189
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 14:54:02 -0400

    EmailService class implements the Notification interface

---
+++ MyTube/src/com/margietube/EmailService.java
@@ -0,0 +1,10 @@
+package com.margietube;
+
+public class EmailService implements NotificationService {
+    @Override
+    public void notify(User user) {
+        System.out.println("Notifying " + user.getEmail() + "...");
+
+        System.out.println("Done!\n");
+    }
+}
