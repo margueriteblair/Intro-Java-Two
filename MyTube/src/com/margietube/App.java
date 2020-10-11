1680c1e9fe326a58f08d61e5456bfcdaa604b754
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 14:52:31 -0400

    app java up to mosh standards

--- MyTube/src/com/margietube/App.java
+++ MyTube/src/com/margietube/App.java
@@ -1,7 +1,13 @@
 package com.margietube;
-
+//most of this program is to practice appropriately placing interfaces
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        var video = new Video(); //create new instance of a video
+        video.setFileName("birthday.mp4");
+        video.setTitle("Margie's Birthday!");
+        video.setUser(new User("margie@gmail.com"));
+
+        var processor = new VideoProcessor(new XvideoEncoder(), new SqlVideoDataBase(), new EmailService());
+        processor.process(video);
     }
 }
