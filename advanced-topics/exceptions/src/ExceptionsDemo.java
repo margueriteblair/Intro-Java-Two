64aa4f1333b0c73a88c076bd1d2a697b98170760
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 23:20:24 -0400

    you can have exceptions passed in from the try block

--- advanced-topics/exceptions/src/ExceptionsDemo.java
+++ advanced-topics/exceptions/src/ExceptionsDemo.java
@@ -1,26 +1,29 @@
 package src;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try{
-            reader = new FileReader("file.txt");
+        try (var reader = new FileReader("file.txt");
+             var writer = new FileWriter("...");
+        ) {
+            //try with resources statement
+            //when we compile this code,it will add a finally block
             var value = reader.read();
         } 
         catch (IOException e){ //with this format you can handle multiple exceptions at the same time
             System.out.println("Could not read data.");
         }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException ex){
-                    ex.printStackTrace();
-                }
-            }
-        }
+        // finally {
+        //     if (reader != null) {
+        //         try {
+        //             reader.close();
+        //         } catch (IOException ex){
+        //             ex.printStackTrace();
+        //         }
+        //     }
+        // }
     }
 }
