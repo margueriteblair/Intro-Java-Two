70d999d891cded0de2da70462e21c6ec57c59d2e
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 23:15:18 -0400

    try catch blocks for checked exceptions

--- advanced-topics/exceptions/src/ExceptionsDemo.java
+++ advanced-topics/exceptions/src/ExceptionsDemo.java
@@ -1,13 +1,31 @@
 package src;
 
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+
 public class ExceptionsDemo {
     public static void show() {
-        var reader = new FileReader("file.txt");
-        //java compiler knows that it'll throw an exception
-        //checked exceptionsare ones we should anticipate
-    }
-
-    public static void sayHello(String name) {
-        System.out.println(name.toUpperCase());
+        var reader = new FileReader();
+        try{
+            reader = new FileReader("file.txt");
+            var value = reader.read();
+        } 
+        catch (IOException | ParseException e){ //with this format you can handle multiple exceptions at the same time
+            System.out.println("Could not read data.");
+        }
+        catch (ClassNotFoundException ex) {
+            System.out.println("Failed to lead the appropriate classes.");
+        }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
--- advanced-topics/exceptions/src/Main.java
+++ advanced-topics/exceptions/src/Main.java
@@ -3,6 +3,5 @@ package src;
 public class Main {
     public static void main(String[] args) {
         ExceptionsDemo.show();
-        //imported the show method
     }
 }
