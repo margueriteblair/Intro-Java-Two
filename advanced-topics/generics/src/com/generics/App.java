5d0be1816c653307592be31ebbe25712cd3794f5
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-12 01:08:14 -0400

    created a list

---
+++ advanced-topics/generics/src/com/generics/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var list = new List();
+        list.add(1);
+    }
+}
---
+++ advanced-topics/generics/src/com/generics/UserList.java
@@ -0,0 +1,5 @@
+public class UserList {
+    private User[] items = new User[10];
+
+    private int count;
+}
