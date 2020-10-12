8e19905904d7c4aa0f48574c477bd209b97c68cb
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-12 19:30:15 -0400

    setting up environmnt for mosh course on collections

---
+++ advanced-topics/collections/src/GenericList.java
@@ -0,0 +1,7 @@
+package src;
+
+public class GenericList {
+    public void hello() {
+        System.out.println("testing for generic list");
+    }
+}
---
+++ advanced-topics/collections/src/Main.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        var list = new GenericList();
+        list.hello();
+    }
+}
