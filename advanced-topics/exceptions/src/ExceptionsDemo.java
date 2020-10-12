fe9e9adefc56d957392a7961295b461cb470e37c
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 21:51:31 -0400

    notes on checked, runtime, and errors

--- advanced-topics/exceptions/src/ExceptionsDemo.java
+++ advanced-topics/exceptions/src/ExceptionsDemo.java
@@ -2,6 +2,12 @@ package src;
 
 public class ExceptionsDemo {
     public static void show() {
-        
+        var reader = new FileReader("file.txt");
+        //java compiler knows that it'll throw an exception
+        //checked exceptionsare ones we should anticipate
+    }
+
+    public static void sayHello(String name) {
+        System.out.println(name.toUpperCase());
     }
 }
