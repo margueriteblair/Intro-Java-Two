cbbfdfcf3735fd8a6eebd40781a2d208c39d346e
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-12 14:49:05 -0400

    creating generic methods

--- advanced-topics/generics/src/Main.java
+++ advanced-topics/generics/src/Main.java
@@ -2,13 +2,6 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        GenericList<Integer> numbers = new GenericList<Integer>(); //cannot pass in int, float
-        //every primitive type has a wrapper class
-        //int -> Integer
-        //float -> Float
-        //boolean -> Boolean
-        numbers.add(1); //boxing, java compiler will put this value inside a box
-        int numb = numbers.get(0); //unboxing
-        System.out.println(numb);
+        var max = Utils.max(1, 4);
     }
 }
---
+++ advanced-topics/generics/src/Utils.java
@@ -0,0 +1,5 @@
+public class Utils {
+    public static <T extends Comparable<T>> T max(T first, T second) {
+        return (first.compareTo(second) < 0) ? second : first;
+    }
+}
