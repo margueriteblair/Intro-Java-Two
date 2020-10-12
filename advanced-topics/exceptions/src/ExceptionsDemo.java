56b51d4c3159be85ce765d96b3408ad451d616ba
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 23:16:45 -0400

    fully read through try catch block

--- advanced-topics/exceptions/src/ExceptionsDemo.java
+++ advanced-topics/exceptions/src/ExceptionsDemo.java
@@ -1,22 +1,17 @@
 package src;
 
 import java.io.FileReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.ParseException;
 
 public class ExceptionsDemo {
     public static void show() {
-        var reader = new FileReader();
+        FileReader reader = null;
         try{
             reader = new FileReader("file.txt");
             var value = reader.read();
         } 
-        catch (IOException | ParseException e){ //with this format you can handle multiple exceptions at the same time
+        catch (IOException e){ //with this format you can handle multiple exceptions at the same time
             System.out.println("Could not read data.");
-        }
-        catch (ClassNotFoundException ex) {
-            System.out.println("Failed to lead the appropriate classes.");
         }
         finally {
             if (reader != null) {
