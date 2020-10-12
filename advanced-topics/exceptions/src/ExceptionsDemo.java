519bcc7642b2902c99b83edb4bff8db80b54123d
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-11 23:38:10 -0400

    try-catch to handle an IO error

--- advanced-topics/exceptions/src/ExceptionsDemo.java
+++ advanced-topics/exceptions/src/ExceptionsDemo.java
@@ -1,29 +1,15 @@
 package src;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
-        try (var reader = new FileReader("file.txt");
-             var writer = new FileWriter("...");
-        ) {
-            //try with resources statement
-            //when we compile this code,it will add a finally block
-            var value = reader.read();
-        } 
-        catch (IOException e){ //with this format you can handle multiple exceptions at the same time
-            System.out.println("Could not read data.");
+        try {
+            var account = new Account();
+            account.deposit(-1);
         }
-        // finally {
-        //     if (reader != null) {
-        //         try {
-        //             reader.close();
-        //         } catch (IOException ex){
-        //             ex.printStackTrace();
-        //         }
-        //     }
-        // }
+        catch (IOException ex) {
+            System.out.println("There is an error!");
+        }
     }
 }
