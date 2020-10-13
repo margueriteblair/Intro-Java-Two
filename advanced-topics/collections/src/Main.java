9d1b26b0c7a16397de5d5a2ce09da0bb202fa535
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-13 00:03:26 -0400

    comparator interface

--- advanced-topics/collections/src/Main.java
+++ advanced-topics/collections/src/Main.java
@@ -1,6 +1,5 @@
 package src;
 
-import src.GenericList;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,10 +7,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
         List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("a"));
-        customers.add(new Customer("ab"));
-        customers.add(new Customer("abc"));
-        Collections.sort(customers);
+        customers.add(new Customer("a", "a@gmail.com"));
+        customers.add(new Customer("ab", "ab@gmail.com"));
+        customers.add(new Customer("abc", "abc@gmail.com"));
+        Collections.sort(customers, new EmailComparator());
         System.out.println(customers);
 
     }
