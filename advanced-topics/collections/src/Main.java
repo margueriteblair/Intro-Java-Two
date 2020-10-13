561cb09cb7cd6d98ea452a285e109d9600433718
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-12 21:38:04 -0400

    used the iterable interface to loop through objects

--- advanced-topics/collections/src/Main.java
+++ advanced-topics/collections/src/Main.java
@@ -1,9 +1,18 @@
 package src;
 
+import src.GenericList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        var list = new GenericList();
-        list.hello();
+        var list = new GenericList<String>();
+        var iterator = list.iterator(); //with this we can loop over our list without knowing anything about its internal implementation
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            System.out.println(current);
+            //iterable way to circle through a list
+        }
+        for (var item:list) {
+            System.out.println(item);
+        } //the for each loop is syntactical sugar over the for each loop
     }
 }
