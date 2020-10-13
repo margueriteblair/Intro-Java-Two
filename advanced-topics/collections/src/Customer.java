106421b988e7a4d8941259e30ab46aa5185dbc0b
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-12 23:36:20 -0400

    comparable interface to compare and sort data

---
+++ advanced-topics/collections/src/Customer.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Customer implements Comparable<Customer> {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+    @Override
+    public int compareTo(Customer other) {
+        //we compare to two objects
+        return name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- advanced-topics/collections/src/Main.java
+++ advanced-topics/collections/src/Main.java
@@ -1,9 +1,18 @@
 package src;
 
 import src.GenericList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ListDemo.show();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("a"));
+        customers.add(new Customer("ab"));
+        customers.add(new Customer("abc"));
+        Collections.sort(customers);
+        System.out.println(customers);
+
     }
 }
