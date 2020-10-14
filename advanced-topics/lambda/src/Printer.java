45c81508eb81e0c15bc8070cdee4a9dddcc287f3
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 01:47:10 -0400

    lambda expressions to represent anonymous functions

--- advanced-topics/lambda/src/Printer.java
+++ advanced-topics/lambda/src/Printer.java
@@ -2,7 +2,7 @@ package src;
 
 
 public interface Printer {
-    void print(String message); //a single abstract method
+    void print(String message); //a single abstract method, functional method
 
     //interfaces should not have implmenetation only be a contract
 }
