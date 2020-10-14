002e71eaf57fca794384c582b21ee87e577be50c
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 02:09:32 -0400

    method references to write very concise lambda expressions

--- advanced-topics/lambda/src/LambdasDemo.java
+++ advanced-topics/lambda/src/LambdasDemo.java
@@ -1,13 +1,19 @@
 package src;
 
 public class LambdasDemo {
-    String prefix = "-";
     public static void show() {
-        greet(message -> {System.out.println(message);});
 
-        Printer print = new ConsolePrinter();
-        Printer printer = message -> System.out.println(message + prefix);
+        greet(message -> {System.out.println(message);});
+        greet(System.out::println); //this line is equivalent to the above line
+        //Class/Object::method
+        greet(message -> print(message));
+        greet(LambdasDemo::print);
+        var demo =new LambdasDemo();
+        greet(message -> demo.print(message));
+        greet(demo::print);
     }
+    public void print(String message) {}
+
 
     public static void greet(Printer printer) {
         printer.print("Hello world");
