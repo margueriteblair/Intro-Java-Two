02bc67b1a35ad4944c8ab83ef57a6674af38859d
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 18:34:24 -0400

    using method references from comparator to sort movie titles

--- advanced-topics/streams/src/CreatingStreamsDemo.java
+++ advanced-topics/streams/src/CreatingStreamsDemo.java
@@ -3,6 +3,7 @@ package src;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.jar.Attributes.Name;
@@ -11,14 +12,14 @@ import java.util.stream.Stream;
 public class CreatingStreamsDemo {
     public static void show() {
         var movies = List.of(
-            new Movie("a", 10),
-            new Movie("b", 20),
+            new Movie("b", 10),
+            new Movie("a", 20),
             new Movie("c", 30)
         );
-        movies.stream()
-        .skip(2)
-        .forEach(m->System.out.println(m.getTitle()));
-
-        movies.stream().takeWhile(m -> m.getLikes() < 30).forEach(m -> System.out.println(m.getTitle()));
+        //sorted(Comparator.comparing(m -> m.getTitle()))
+        //sorted(Comparator.comparing(Movie::getTitle))
+        movies.stream().sorted((a, b) -> a.getTitle().compareTo(b.getTitle())).forEach(m -> System.out.println(m.getTitle()));
+        System.out.println("Method ref:");
+        movies.stream().sorted(Comparator.comparing(Movie::getTitle)).forEach(m -> System.out.println(m.getTitle()));
     }
 }
