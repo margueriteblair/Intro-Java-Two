f4f06d32882667a65911252e58a251708f829e0e
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 02:29:48 -0400

    lambda function in conjunction with a consumer interface

--- advanced-topics/lambda/src/LambdasDemo.java
+++ advanced-topics/lambda/src/LambdasDemo.java
@@ -1,21 +1,17 @@
 package src;
 
+import java.util.List;
+
 public class LambdasDemo {
     public static void show() {
-
-        greet(message -> {System.out.println(message);});
-        greet(System.out::println); //this line is equivalent to the above line
-        //Class/Object::method
-        greet(message -> print(message));
-        greet(LambdasDemo::print);
-        var demo =new LambdasDemo();
-        greet(message -> demo.print(message));
-        greet(demo::print);
+        List<Integer> list = List.of(1, 2, 3);
+        //imperative programming
+        for (var item: list){
+            System.out.println(item);
+        }
+        //Declarative programming
+        //there are a lot if different programming paradigms;
+        list.forEach((item) -> {System.out.println(item);});
     }
-    public void print(String message) {}
 
-
-    public static void greet(Printer printer) {
-        printer.print("Hello world");
-    }
 }
