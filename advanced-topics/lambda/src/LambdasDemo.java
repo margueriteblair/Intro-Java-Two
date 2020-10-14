42dd2e47d29e657cdeacb20d2274871963a45fd5
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 01:58:01 -0400

    this keyword in lambdas function

--- advanced-topics/lambda/src/LambdasDemo.java
+++ advanced-topics/lambda/src/LambdasDemo.java
@@ -1,8 +1,12 @@
 package src;
 
 public class LambdasDemo {
+    String prefix = "-";
     public static void show() {
         greet(message -> {System.out.println(message);});
+
+        Printer print = new ConsolePrinter();
+        Printer printer = message -> System.out.println(message + prefix);
     }
 
     public static void greet(Printer printer) {
