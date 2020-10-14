6dabf5d928f8be9930294f7c32735d03f0f16981
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 02:52:31 -0400

    consumer method printing chaining

--- advanced-topics/lambda/src/LambdasDemo.java
+++ advanced-topics/lambda/src/LambdasDemo.java
@@ -1,17 +1,13 @@
 package src;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class LambdasDemo {
     public static void show() {
-        List<Integer> list = List.of(1, 2, 3);
-        //imperative programming
-        for (var item: list){
-            System.out.println(item);
-        }
-        //Declarative programming
-        //there are a lot if different programming paradigms;
-        list.forEach((item) -> {System.out.println(item);});
+        List<String> list = List.of("a", "b", "c");
+        Consumer<String> print = (String item) -> System.out.println(item);
+        
     }
 
 }
