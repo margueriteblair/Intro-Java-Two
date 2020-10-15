98020f4bcb5b1a45973ee2fb9fe4d8b39f3df3e2
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-15 15:16:46 -0400

    created a completable future

---
+++ advanced-topics/executive-framework/src/CompletableFuturesDemo.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Supplier;
+
+public class CompletableFuturesDemo {
+    public static void show() {
+        Supplier<Integer> task = () -> 1;
+        var future = CompletableFuture.supplyAsync(task);
+        try {
+            var result = future.get();
+            System.out.println(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
