21fd122fae07fbeba5ed50b7aa10258681d2b37f
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-15 14:07:43 -0400

    put the executor shutdown command in the finally block

--- advanced-topics/executive-framework/src/ExecutorsDemo.java
+++ advanced-topics/executive-framework/src/ExecutorsDemo.java
@@ -4,8 +4,15 @@ import java.util.concurrent.Executors;
 public class ExecutorsDemo {
     public static void show() {
         var executor = Executors.newFixedThreadPool(2); //new fixed thread pool and we pass in two threads
-        executor.submit(() -> {
-            System.out.println(Thread.currentThread().getName());
-        })
+        
+        try {
+            executor.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
+            });
+
+        } finally {
+            executor.shutdown();
+
+        }
     }
 }
