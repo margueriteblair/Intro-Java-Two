c0ef988a6356bf7a009aae59ed3a26d7bd699054
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 21:57:24 -0400

    interrupting a thread running

--- advanced-topics/concurrency/src/DownloadFileTask.java
+++ advanced-topics/concurrency/src/DownloadFileTask.java
@@ -4,10 +4,9 @@ public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading a file..." + Thread.currentThread().getName());
-        try{
-            Thread.sleep(5000); //suspends the current thread for 5 seconds or 5000 milliseconds
-        } catch(InterruptedException e) {
-            e.printStackTrace();
+        for (var i = 0; i < Integer.MAX_VALUE; i++) {
+            if (Thread.currentThread().isInterrupted()) return;
+            System.out.println("Downloading byte " + i);
         }
         System.out.println("Download complete" + Thread.currentThread().getName());
     }
--- advanced-topics/concurrency/src/ThreadDemo.java
+++ advanced-topics/concurrency/src/ThreadDemo.java
@@ -10,11 +10,12 @@ public class ThreadDemo {
             thread.start();
 
             try {
-                thread.join();
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("File is ready to be scanned.");
+
+            thread.interrupt();
         }
     }
 }
