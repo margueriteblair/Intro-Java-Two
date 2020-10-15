e7049e9d1a91c04d74572d3857159adc4592c536
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-14 21:39:43 -0400

    joining a thread with the join method

--- advanced-topics/concurrency/src/ThreadDemo.java
+++ advanced-topics/concurrency/src/ThreadDemo.java
@@ -8,6 +8,13 @@ public class ThreadDemo {
         for (int i = 0; i < 10; i++) {
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
+
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("File is ready to be scanned.");
         }
     }
 }
