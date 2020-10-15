898a43c0845d50e6f3de6829affa38ba25286e9d
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-15 02:00:04 -0400

    lock class to synchronize a program

--- advanced-topics/concurrency/src/DownloadStatus.java
+++ advanced-topics/concurrency/src/DownloadStatus.java
@@ -1,14 +1,25 @@
 package src;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
     private int totalBytes;
+    private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
     public void incrementTotalBytes() {
-        totalBytes++;
+        lock.lock();
+        try {
+            totalBytes++;
+        } finally{
+            lock.unlock();
+        }
+        //lock and unlock method prevent other methods from using the same one
+        //good practice to have the finally block to unlock the lock
     }
     //no set total bytes method because it always starts at 0, and we want to increment it
     //we don't want someone to be able to manually reset the byte count
