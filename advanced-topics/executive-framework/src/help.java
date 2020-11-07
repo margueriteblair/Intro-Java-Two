84771a61c20d6dd526c6b38acfb9fadcfaedfea6
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-06 19:46:53 -0500

    bias algo

--- advanced-topics/executive-framework/src/help.java
+++ advanced-topics/executive-framework/src/help.java
@@ -1,4 +1,31 @@
-public class help {
-    double dub = 10.0 / -0;
-    System.out.println(dub);
+package com.margieblair;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        List<Integer> test = new ArrayList<>();
+        test.add(3);
+        test.add(6);
+        test.add(6);
+        test.add(7);
+        minimumBias(test);
+    }
+
+
+    public static void minimumBias(List<Integer> ratings) {
+        int totalBias = 0;
+        Integer[] ratingsStream = ratings.stream().sorted().toArray(Integer[]::new);
+        System.out.println(Arrays.toString(Arrays.stream(ratingsStream).toArray()));
+        for (int i = 0; i < ratingsStream.length; i+=2) {
+            System.out.println();
+            totalBias += (ratingsStream[i+1] - ratingsStream[i]);
+        }
+        System.out.println(totalBias);
+    }
 }
