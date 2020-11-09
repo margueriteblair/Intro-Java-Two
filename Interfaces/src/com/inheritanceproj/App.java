1f829e2f5cd68fdbdc9cf5d505589b2056d7e2f0
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 18:21:57 -0500

    hello world

--- Interfaces/src/com/inheritanceproj/App.java
+++ Interfaces/src/com/inheritanceproj/App.java
@@ -4,8 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         var calculator = new TaxCalculator2018(100_000);
         var report = new TaxReport(calculator);
-        report.show(calculator);
-        report.show(new TaxCalculator2019());
 
         // report.setCalculator(new TaxCalculator2019());
         // report.show();
