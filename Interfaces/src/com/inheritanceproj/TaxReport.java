9d4317b2be104dca5a97faf519e904904ac17103
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 18:32:22 -0500

    the different types of dependency injection

--- Interfaces/src/com/inheritanceproj/TaxReport.java
+++ Interfaces/src/com/inheritanceproj/TaxReport.java
@@ -6,11 +6,13 @@ public class TaxReport {
     // public TaxReport(TaxCalculator calculator) {
     //     this.calculator = calculator; //we're injecting the dependency via  a construcotr;
     // }
+            //this right here is constructor injection, as opposed to saying:
+        //this.calculator = new TaxCalculator();
 
     public void show(TaxCalculator calculator) {
         this.calculator = calculator;
-        //this right here is constructor injection, as opposed to saying:
-        //this.calculator = new TaxCalculator();
+        //this right here!!! is method injection, we're injecting our dependencies within a method that belongs to this class
+        //we can also inject depedencies into setter and constructors
     }
 
     public void setCalculator(TaxCalculator calculator) {
