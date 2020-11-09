c8601c12695ac5529add8b54dcb715cfc3e28d04
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 18:28:41 -0500

    setter injection

--- Interfaces/src/com/inheritanceproj/App.java
+++ Interfaces/src/com/inheritanceproj/App.java
@@ -4,6 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         var calculator = new TaxCalculator2018(100_000);
         var report = new TaxReport(calculator);
+        report.show();
+
+        report.setCalculator(new TaxCalculator2019());
 
         // report.setCalculator(new TaxCalculator2019());
         // report.show();
--- Interfaces/src/com/inheritanceproj/TaxReport.java
+++ Interfaces/src/com/inheritanceproj/TaxReport.java
@@ -1,18 +1,19 @@
 package com.inheritanceproj;
 
 public class TaxReport {
-    private TaxCalculator calculator;
+    private TaxCalculator calculator; //we want to make an instance of the interace TaxCalculator
 
     // public TaxReport(TaxCalculator calculator) {
     //     this.calculator = calculator; //we're injecting the dependency via  a construcotr;
     // }
 
     public void show(TaxCalculator calculator) {
-        var tax = calculator.calculateTax();
-        System.out.println(tax);
+        this.calculator = calculator;
+        //this right here is constructor injection, as opposed to saying:
+        //this.calculator = new TaxCalculator();
     }
 
-    // public void setCalculator(TaxCalculator calculator) {
-    //     this.calculator = calculator;
-    // }
+    public void setCalculator(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
 }
